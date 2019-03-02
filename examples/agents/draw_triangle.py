f06fef67e9ab3ed710a6b120a0544358944e582f
ivanna <ivannamyckhal@uni-koblenz.de>
2019-03-02 22:36:14 +0100

    new reward and new model for drawing_triangle

--- examples/agents/draw_triangle.py
+++ examples/agents/draw_triangle.py
@@ -2,14 +2,6 @@ import gym
 import numpy as np
 from gym.spaces import Box, Discrete, Dict
 
-
-print('Current gym version =', gym.__version__)
-env = gym.make('FetchDrawTriangle-v1')
-#env = gym.make('FetchPush-v1')
-
-obs = env.reset()
-print('Observation space= ', env.observation_space)
-print('Action space=', env.action_space.shape)
 """{'observation': array([ 1.31642683e+00,  3.94268249e-01,  7.01529477e-01,  1.62232554e+00,
         6.92140471e-01,  2.48922446e-02,  3.05898702e-01,  2.97872223e-01,
        -6.76637232e-01,  5.03188288e-02,  5.03330159e-02, -1.57079633e+00,
@@ -20,22 +12,45 @@ print('Action space=', env.action_space.shape)
     'achieved_goal': array([1.62232554, 0.69214047, 0.02489224]), 
     'desired_goal': array([1.47274893, 0.65727962, 0.86690607])}
 """
-obs_arr = np.empty([31])
-is_dict = 'dict' in str(type(obs))
-print('Is dict?', is_dict)
 
-#for key, value in obs.items():
-	#value = value.reshape((value.shape[0]))
-	#np.concatenate((obs_arr, value))
+print('Current gym version =', gym.__version__)
+env = gym.make('FetchDrawTriangle-v1')
+obs = env.reset()
+print('Observation space= ', env.observation_space)
+print('Action space=', env.action_space.shape)
+print('Max episode length ', env._max_episode_steps)
 
-#obs_arr =  obs_arr.reshape((obs_arr.shape[1], obs_arr.shape[0] ))
-#print(obs_arr.reshape((obs_arr.shape[1],obs_arr.shape[0] )))
+desired_goals  = np.array(obs['desired_goal'])
+desired_goals = desired_goals.reshape((9,3))
+achieved_goals = np.array(obs['achieved_goal'])
+achieved_goals = achieved_goals.reshape((9,3))
+
+print('desired_goals=', desired_goals)
+i = 0
+ep_length = 0 
 
 while True:
-	env.render()
-	
-	a = env.action_space.sample()
-	#print('Sampled a=', a)
-	obs, reward, done, info = env.step(a)
-	print(obs)
-	#print('New observation=', obs)
+    ep_length +=1
+    #print('i=',i)
+    env.render()
+    cur_grip_pos = obs['observation'][0:3]
+    #print(cur_grip_pos)
+    a = desired_goals[i,:] - cur_grip_pos
+    act = [a[0], a[1], a[2], 0.15]
+    #print(a)
+    
+    #a = env.action_space.sample()
+    #print('Sampled a=', a)
+    obs, reward, done, info = env.step(act)
+    achieved_goals = np.array(obs['achieved_goal'])
+    achieved_goals = achieved_goals.reshape((9,3))
+    #print('Reward = ', reward)
+    #print('Info=', info)
+    done = info['is_success']
+    if done:
+        print('Episode length = ', ep_length)
+        break
+    
+    if np.sum(achieved_goals[i,:]) > 0.0 and not done:
+        i = i+1
+    #print('New observation=', obs)
--- gym/envs/robotics/fetch/draw_triangle.py
+++ gym/envs/robotics/fetch/draw_triangle.py
@@ -3,6 +3,7 @@ import gym
 from gym import utils
 from gym.envs.robotics import fetch_env, rotations, robot_env
 import numpy as np
+import copy
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 
@@ -51,6 +52,7 @@ class Goal():
         print('Position = {}, id = {}, reached = {} '.format(self.position, self.id, self.reached))
 
 
+
 class FetchDrawTriangleEnv(fetch_env.FetchEnv, utils.EzPickle):
     def __init__(self, reward_type='sparse', num_goals=9):
         initial_qpos = {
@@ -59,53 +61,86 @@ class FetchDrawTriangleEnv(fetch_env.FetchEnv, utils.EzPickle):
             'robot0:slide2': 0.0,
         }
 
+        self.initial_goals = {
+                    'goal0': [1.20727302, 0.5951655 , 0.49858342], 
+                    'goal1': [1.22474433, 0.67346458, 0.64335482], 
+                    'goal2': [1.22474433, 0.6348449 , 0.56827542], 
+                    'goal3': [1.22474433, 0.723543  , 0.71355354], 
+                    'goal4': [1.22474434, 0.76215388, 0.6341726 ], 
+                    'goal5': [1.22474433, 0.80084932, 0.56561652], 
+                    'goal6': [1.22474433, 0.85758673, 0.5013173 ], 
+                    'goal7': [1.22474433, 0.76535415, 0.50012584], 
+                    'goal8': [1.22474433, 0.68332691, 0.49927819]
+                    }
+
         self.num_goals = num_goals
         self.goals = []
         self.ep_reward = 0
-        self.reach_factor = 0.75
+        self.reach_factor = 1.0
         self.last_reached_goal = -1
+        self.names_to_pos = {}
+
 
         print('MODEL_XML_PATH', MODEL_XML_PATH)
         fetch_env.FetchEnv.__init__(
             self, MODEL_XML_PATH, has_object=False, block_gripper=True, n_substeps=20,
             gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
-            obj_range=0.15, target_range=0.15, distance_threshold=0.1,
+            obj_range=0.15, target_range=0.15, distance_threshold=0.05,
             initial_qpos=initial_qpos, reward_type=reward_type, gripper_rot=[1., 0., 0., 0.])
         utils.EzPickle.__init__(self)
-        #self.initial_gripper_xpos = self.sim.data.get_site_xpos('robot0:grip').copy()
-        self._sample_goals()
+        self.names_to_pos = copy.deepcopy(self.init_goals()) 
+        print('init self.names_to_pos', self.names_to_pos)
+        self.initial_gripper_xpos = self.sim.data.get_site_xpos('robot0:grip').copy()
+       # self._sample_goals()
 
+
+  #  def _render_callback(self):
+  #      # Visualize target.
+  #      sites_offset = (self.sim.data.site_xpos - self.sim.model.site_pos).copy()       
+  #      site_id = self.sim.model.site_name2id('target0')
+  #      self.sim.model.site_pos[site_id] = self.goal - sites_offset[0]
+  #      self.sim.forward()
 
 
     def _render_callback(self):
-        print('Rendering callback')
+        #print('Rendering callback')
         sites_offset = (self.sim.data.site_xpos - self.sim.model.site_pos).copy()
+
         for i in range(self.num_goals):
             site_id = self.sim.model.site_name2id('target'+str(i))
+            #print('sites_offset[i]', sites_offset[i] )
+            #print('self.goals[i].position ', self.goals[i].position )
             self.sim.model.site_pos[site_id] =  self.goals[i].position - sites_offset[i] 
-       #     print('self.sim.model.site_pos[site_id]', self.sim.model.site_pos[site_id])
+            #print('self.sim.model.site_pos[site_id]', self.sim.model.site_pos[site_id])
         self.sim.forward()
 
 
-
-    def _sample_goals(self):
-        print('--- Sampling goals ---')
+    def init_goals(self):
         body_names = self.sim.model.body_names
         body_pos = self.sim.model.body_pos
-        names_to_pos = ids_to_pos(body_names, body_pos)
+        #names_to_pos = ids_to_pos(body_names, body_pos)
+        return ids_to_pos(body_names, body_pos)
+
+    
+
+
+    def _sample_goals(self):
         self.goals = []
-        for k,v in names_to_pos.items():
-            random = np.random.uniform(-0.015, 0.015, size=3)
-            v[0] =  self.initial_gripper_xpos[0] + v[0]/3.5 + random[0]
-            v[1] =  self.initial_gripper_xpos[1] + v[1]/1.2 + random[1]
-            v[2] =  self.initial_gripper_xpos[2] + v[2]/2.5 + random[2]      
-            goal_obj = Goal(k, v, False)
+       # print('Pos in model ', self.init_goals())
+        for k,v in self.initial_goals.items():
+            random = np.random.uniform(-0.005, 0.005, size=3)
+            v[0] =  v[0] #0.7 * (v[0]  + random[0]) + 0.6
+            v[1] =  v[1] + random[1] #0.7 * (v[1]  + random[1]) + 0.4
+            v[2] =  v[2] + random[2] #0.7 * (v[2] * 0.8  + random[2]) + 0.3
+            g_id = int(k.split('goal')[1])     
+            goal_obj = Goal(g_id, v, False)
             self.goals.append(goal_obj)
         return self.goals
 
 
 
     def _get_obs(self):
+       # self._sample_goals()
         desired_goals = []
         achieved_goals = []
         grip_pos = self.sim.data.get_site_xpos('robot0:grip')
@@ -123,6 +158,7 @@ class FetchDrawTriangleEnv(fetch_env.FetchEnv, utils.EzPickle):
         else:
             for g in self.goals:
                 goal = [g.position[0], g.position[1], g.position[2]]
+                #print('Goal with id=',g.id, ' has a position=', goal)
                 for g1 in goal:
                     desired_goals.append(g1)
                 
@@ -141,6 +177,7 @@ class FetchDrawTriangleEnv(fetch_env.FetchEnv, utils.EzPickle):
         }
 
 
+
     def _is_success(self, achieved_goal, desired_goal):
         '''
         Hard condition: Success is True, when all of the goals are reached
@@ -148,39 +185,57 @@ class FetchDrawTriangleEnv(fetch_env.FetchEnv, utils.EzPickle):
         reward with additinal num_goals if all goals are reached
         '''
         has_to_be_reached = self.reach_factor * self.num_goals
-
         if has_to_be_reached <= self.ep_reward:
             print('Consider as done! Reward=', self.ep_reward)
         return has_to_be_reached <= self.ep_reward
 
 
+
+
+    def reset(self):
+        # Attempt to reset the simulator. Since we randomize initial conditions, it
+        # is possible to get into a state with numerical issues (e.g. due to penetration or
+        # Gimbel lock) or we may not achieve an initial condition (e.g. an object is within the hand).
+        # In this case, we just keep randomizing until we eventually achieve a valid initial
+        # configuration.
+        self._sample_goals()
+        did_reset_sim = False
+        while not did_reset_sim:
+            did_reset_sim = self._reset_sim()
+
+        obs = self._get_obs()
+        self.ep_reward = 0
+        self.last_reached_goal = -1
+        return obs
+
+        
     def compute_reward(self, achieved_goal, goal, info):
         # Compute distance between goal and the achieved goal.
         # todo: penalize if the arm is to far from the goal 
         #store in self.last_reached_goal id of the last reached goal
         reward = self.ep_reward
-        print('Computing reward for drawing trinagle')
+       # print('Computing reward for drawing trinagle')
         grip_pos = self.sim.data.get_site_xpos('robot0:grip')
         cur_grip_position = self.sim.data.get_site_xpos('robot0:grip')
-        print('distance_threshold', self.distance_threshold)
         
         for i in range(len(self.goals)):
             goal = self.goals[i]
-            print('Considering goal = ', goal.id, ' is goal reached? = ', goal.reached)
             if not goal.reached and goal.id == (self.last_reached_goal + 1):
+        #        print('Considering goal = ', goal.id, ' is goal reached? = ', goal.reached)
                 dist_to_goal = distance_goal(grip_pos, goal.position)
-                print('Dist to goal=', dist_to_goal)
+         #       print('Dist to goal=', dist_to_goal)
                 if dist_to_goal < self.distance_threshold:
-                    print('Reached goal with id =', goal.id)
+          #          print('Reached goal with id =', goal.id)
                     self.goals[i].reached = True
                     self.last_reached_goal = goal.id
-                    reward +=1
-                    break
+                    self.ep_reward +=1
                 else:
                     reward = -1 * dist_to_goal
+                break
 
 
-        self.ep_reward = reward
-        print('Reward = ', reward)
+        reward = self.ep_reward + reward
+        #print('Reward = ', reward)
+        #print('Episode Reward = ', self.ep_reward)
         return reward
  
